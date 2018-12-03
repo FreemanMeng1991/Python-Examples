0270f689e5152b253f5afeae3efec5c37eb2dc2a
FreemanMeng1991 <45162325+FreemanMeng1991@users.noreply.github.com>
2018-12-03 20:23:52 +0800

    Update cache.py

--- cache.py
+++ cache.py
@@ -5,4 +5,23 @@ offset = len(root_path.split("\\"))
 for root,dirs,files in os.walk(root_path):
 	current_dir = root.split("\\")
 	indent = len(current_dir)-offset
-	print("\t"*indent,current_dir[-1])
+	print("\t"*indent,current_dir[-1]
+	      
+**************************************************	      
+from chardet import detect
+count,blanks = 0,0
+with open("a.txt",'rb') as fp:
+	encode = detect(fp.read())['encoding']
+	print(encode)
+	fp.close()
+
+with open("a.txt",'r',encoding=encode) as fp:
+	while True:
+		line = fp.readline()
+		if not line: #判断文件是否结束
+			break
+		count+=1
+		if not (len(line.strip())):
+			blanks+=1
+		
+print(count,blanks)	      
